7c2919b0e3c21101ffd363c2d50c9a0be0f002ed
Lormungand <phoenarun1@gmail.com>
2024-10-27 19:42:09 +0300

    Update Home.vue

--- my-vue-app/src/views/Home.vue
+++ my-vue-app/src/views/Home.vue
@@ -3,7 +3,7 @@
     <div class="content">
       <main>
         <div>
-          <h1>Принимайте платежи от пользователей с помощью скинов CS:GO и DOTA 2</h1>
+          <h1><b>Принимайте платежи<br> от пользователей<br> с помощью скинов<br> CS:GO и DOTA 2</b></h1>
         </div>
         <div>
           <button type="button" aria-label="Подключить проект">Подключить проект</button>
@@ -17,7 +17,14 @@
 .container {
     color: black;
     height: calc(100vh - var(--Header-height) - var(--Footer-height));
-    align-items: normal;
+    align-items: normal; 
+    
+    background-image: url(../assets/main/mainbg.png);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    
+    font-size: 64px;
 }
 main{
   
