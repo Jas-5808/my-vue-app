546d595eddd9c5aa17fd207eab64b05a5ae7e857
Lormungand <phoenarun1@gmail.com>
2024-10-27 19:56:13 +0300

    home

--- my-vue-app/src/views/Home.vue
+++ my-vue-app/src/views/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container">
     <div class="content">
+
       <main>
         <div>
           <h1><b>Принимайте платежи<br> от пользователей<br> с помощью скинов<br> CS:GO и DOTA 2</b></h1>
@@ -9,28 +10,38 @@
           <button type="button" aria-label="Подключить проект">Подключить проект</button>
         </div>
       </main>
+
+       
     </div>
   </div>
 </template>
+
 
 <style scoped>
 .container {
     color: black;
     height: calc(100vh - var(--Header-height) - var(--Footer-height));
+
     align-items: normal; 
     
     background-image: url(../assets/main/mainbg.png);
     background-position: center;
     background-repeat: no-repeat;
     background-size: cover;
+
     
     font-size: 64px;
 }
-main{
+
   
-}
 h1{
   font-size: 56px;
 }
 
+
+
+
+  
+
 </style>
+
